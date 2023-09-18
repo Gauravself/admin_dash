45685137c3d47b02ded8bb964095768199ded21b
Gauravself <rastogi.g1998@gmail.com>
2023-09-18 21:57:12 +0530

    File Setup, Mui Theme and Topography Config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ColorContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Main from "./components/Main";
+
 function App() {
-  return <></>;
+  const routes = createBrowserRouter([
+    {
+      path: "",
+      element: <Main />,
+      index: true,
+      children: [],
+    },
+  ]);
+
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={routes} />
+      </ThemeProvider>
+    </ColorContext.Provider>
+  );
 }
 
 export default App;
