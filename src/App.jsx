23e134711c738ae26fadf90c65b81acce4f7a24e
Gauravself <rastogi.g1998@gmail.com>
2023-09-26 21:32:12 +0530

    Teams Page Integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,101 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { ColorContext, useMode } from "./theme";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import Main from "./components/Main";
+import { ColorModeContext, useMode } from "./theme";
+import TopBar from "./scenes/global/Topbar";
+import SideBar from "./scenes/global/SideBar";
+import Dashboard from "./scenes/dashboard/Dashboard";
+import CircularProgress from "@mui/material/CircularProgress";
+import { tokens } from "./theme";
+import { Box } from "@mui/material";
+
+const Team = React.lazy(() => import("./scenes/team/Team"));
+const Contacts = React.lazy(() => import("./scenes/contacts/Contacts"));
+// import Invoices from "./scenes/dashboard/invoices";
+// import Contacts from "./scenes/dashboard/contacts";
+// import Bar from "./scenes/dashboard/bar";
+// import Form from "./scenes/dashboard/form";
+// import Line from "./scenes/dashboard/line";
+// import Pie from "./scenes/dashboard/pie";
+// import FAQ from "./scenes/dashboard/faq";
+// import Geography from "./scenes/dashboard/geography";
+// import Calendar from "./scenes/dashboard/calender";
+
+// const routes = createBrowserRouter([
+//   // { path: "/", element: <Dashboard /> },
+//   // { path: "/team", element: <Team /> },
+//   // { path: "/contacts", element: <Contacts /> },
+//   // { path: "/invoices", element: <Invoices /> },
+//   // { path: "/bar", element: <Bar /> },
+//   // { path: "/form", element: <Form /> },
+//   // { path: "/line", element: <Line /> },
+//   // { path: "/pie", element: <Pie /> },
+//   // { path: "/faq", element: <FAQ /> },
+//   // { path: "/geography", element: <Geography /> },
+//   // { path: "/calender", element: <Calendar /> },
+// ]);
 
 function App() {
-  const routes = createBrowserRouter([
-    {
-      path: "",
-      element: <Main />,
-      index: true,
-      children: [],
-    },
-  ]);
-
   const [theme, colorMode] = useMode();
+  const colors = tokens(theme.palette.mode);
   return (
-    <ColorContext.Provider value={colorMode}>
+    <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <RouterProvider router={routes} />
+        <div className="app">
+          <SideBar />
+          <main className="content">
+            <TopBar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route
+                path="/team"
+                element={
+                  <React.Suspense
+                    fallback={
+                      <Box
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                      >
+                        <CircularProgress
+                          variant="indeterminate"
+                          sx={{ color: colors.greenAccent[300] }}
+                        />
+                      </Box>
+                    }
+                  >
+                    <Team />
+                  </React.Suspense>
+                }
+              />
+              {/* Contacts */}
+              <Route
+                path="/contact"
+                element={
+                  <React.Suspense
+                    fallback={
+                      <Box
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                      >
+                        <CircularProgress
+                          variant="indeterminate"
+                          sx={{ color: colors.greenAccent[300] }}
+                        />
+                      </Box>
+                    }
+                  >
+                    <Contacts />
+                  </React.Suspense>
+                }
+              />
+            </Routes>
+          </main>
+        </div>
       </ThemeProvider>
-    </ColorContext.Provider>
+    </ColorModeContext.Provider>
   );
 }
-
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { tokens } from "../theme";
+
+// eslint-disable-next-line react/prop-types
+const Header = ({ title, subTitle }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box mb="30px">
+      <Typography
+        variant="h2"
+        color={colors.grey[100]}
+        fontWeight="bold"
+        sx={{ mb: "5px" }}
+      >
+        {title}
+      </Typography>
+      <Typography variant="h5" color={colors.greenAccent[400]}>
+        {subTitle}
+      </Typography>
+    </Box>
+  );
+};
+
+export default Header;
